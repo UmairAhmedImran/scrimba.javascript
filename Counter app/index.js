5ff0deda82b4fad09ff21d1002d996f00ecc8d9e
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-31 20:47:43 +0500

    varabiles

--- Counter app/index.js
+++ Counter app/index.js
@@ -1,1 +1,9 @@
-document.getElementById('count').innerText = 5
+// javascript read from top to bottom so if a variable is printed before decleration it will give an error
+
+// document.getElementById('count').innerText = 5
+
+let count = 0
+console.log(count)
+
+let age = 20 
+console.log(age)
