693d5322f873cd2246cd49199d904e01c0c3c796
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-08-13 23:33:35 +0500

    Adding onclick properties

--- Counter app/index.js
+++ Counter app/index.js
@@ -1,9 +1,5 @@
 // javascript read from top to bottom so if a variable is printed before decleration it will give an error
 
-// document.getElementById('count').innerText = 5
-
-let count = 0
-console.log(count)
-
-let age = 20 
-console.log(age)
+function increment() {
+    console.log("The button is clicked")
+};
