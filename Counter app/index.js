b572ff5bded45b88599087f89110efe3407cebbe
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-08-30 22:09:14 +0500

    Creating a counting function and button to increment it

--- Counter app/index.js
+++ Counter app/index.js
@@ -1,5 +1,7 @@
 // javascript read from top to bottom so if a variable is printed before decleration it will give an error
-
+let count = 0
 function increment() {
-    console.log("The button is clicked")
+    count++;
+    console.log(count)
+    document.getElementById("count").innerText = count
 };
