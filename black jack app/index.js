6202157c5c709c6a07fcc9998e40e7e1780a2884
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-09-03 23:51:59 +0500

    Adding a button

--- black jack app/index.js
+++ black jack app/index.js
@@ -14,15 +14,15 @@ let message = ""
 
 // 2. Reassign the message variable to the string we're logging out
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Black Jack 🥳"
-    hasBlackJack = true
-} else {
-    message = "You are out of the game 😥"
-    isAlive = false
+function save() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Black Jack 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You are out of the game 😥"
+        isAlive = false
+    }
+    console.log(message)
 }
-
-// 3. Log it out!
-console.log(message)
