ecd1bab3f5d016151f0ec491dc758a61be67edec
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-09-03 13:54:27 +0500

    Initializing black jack game app

---
+++ black jack app/index.js
@@ -0,0 +1,9 @@
+// 1. Create two variables, firstCard and secondCard. 
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+
+let firstCard = 5
+let secondCard = 8
+
+let sum = firstCard + secondCard
